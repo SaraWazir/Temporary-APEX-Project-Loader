5544eee23ff8b0e2a312494998352022a2957300
Charles Ross <113279548+CharlesCodyRoss@users.noreply.github.com>
2026-03-23 14:32:14 -0800

    Update information.py

--- tabs/information.py
+++ tabs/information.py
@@ -123,80 +123,246 @@ def manage_information():
     import streamlit as st
     from util.read_only_util import ro_widget
     from util.input_util import (
-        fmt_string, fmt_date, fmt_agol_date,
-        fmt_currency, fmt_int,
+        fmt_string,
+        fmt_date,
+        fmt_agol_date,
+        fmt_currency,
+        fmt_int,
     )
 
-    project = st.session_state.get("project_record", [{}])[0].get("attributes", {})
+    # ------------------------------------------------------------------
+    # Resolve project record
+    # ------------------------------------------------------------------
+    project_rec = st.session_state.get("project_record")
+    if not project_rec:
+        st.warning("No project loaded.")
+        return
+
+    project = project_rec[0].get("attributes", {})
     is_awp = bool(project.get("AWP_Contract_ID"))
 
     # ------------------------------------------------------------------
-    st.markdown("##### PROJECT DATA SOURCE")
-    with st.container(border=True):
-        if is_awp:
-            ro_widget("info_source", "Source", "AASHTOWare")
-            ro_widget("awp_name", "AWP Project", project.get("AWP_Proj_Name"))
-            ro_widget("awp_updated", "Last Updated", fmt_agol_date(project.get("Last_updated_FME")))
-            st.columns(2)[0].button("CHANGE CONNECTION", use_container_width=True, disabled=True)
-            st.columns(2)[1].button("DELETE CONNECTION", use_container_width=True, disabled=True)
-        else:
-            ro_widget("info_source", "Source", "User Input")
-            ro_widget("user_updated", "Last Updated", fmt_agol_date(project.get("EditDate")))
-
+    # PROJECT DATA SOURCE (SEPARATE CONTAINER)
     # ------------------------------------------------------------------
     with st.container(border=True):
+        st.markdown("##### PROJECT DATA SOURCE")
+
+        if is_awp:
+            ro_widget("info_source", "Source", "AASHTOWare")
+
+            c1, c2 = st.columns(2)
+            with c1:
+                ro_widget(
+                    "source_awp_name",
+                    "AASHTOWare Project Name",
+                    fmt_string(project.get("AWP_Proj_Name")),
+                )
+            with c2:
+                ro_widget(
+                    "source_awp_updated",
+                    "Last Updated",
+                    fmt_agol_date(project.get("Last_updated_FME")),
+                )
+
+            b1, b2 = st.columns(2)
+            b1.button("CHANGE CONNECTION", use_container_width=True, disabled=True)
+            b2.button("DELETE CONNECTION", use_container_width=True, disabled=True)
+
+        else:
+            c1, c2 = st.columns(2)
+            with c1:
+                ro_widget("info_source", "Source", "User Input")
+            with c2:
+                ro_widget(
+                    "source_user_updated",
+                    "Last Updated",
+                    fmt_agol_date(project.get("EditDate")),
+                )
+
+    st.write("")
+
+    # ------------------------------------------------------------------
+    # PROJECT INFORMATION (SEPARATE CONTAINER)
+    # ------------------------------------------------------------------
+    with st.container(border=True):
+
+        # --------------------------------------------------------------
         st.markdown("<h5>1. PROJECT NAME</h5>", unsafe_allow_html=True)
         if is_awp:
-            ro_widget("awp_proj_name", "AASHTOWare Project Name", project.get("AWP_Proj_Name"))
-        ro_widget("proj_name", "Public Project Name", project.get("Proj_Name"))
+            c1, c2 = st.columns(2)
+            with c1:
+                ro_widget(
+                    "awp_proj_name",
+                    "AASHTOWare Project Name",
+                    fmt_string(project.get("AWP_Proj_Name")),
+                )
+            with c2:
+                ro_widget(
+                    "proj_name",
+                    "Public Project Name",
+                    fmt_string(project.get("Proj_Name")),
+                )
+        else:
+            ro_widget(
+                "proj_name",
+                "Public Project Name",
+                fmt_string(project.get("Proj_Name")),
+            )
 
+        st.write("")
+
+        # --------------------------------------------------------------
         st.markdown("<h5>2. CONSTRUCTION YEAR, PHASE, & IDS</h5>", unsafe_allow_html=True)
-        ro_widget("construction_year", "Construction Year", project.get("Construction_Year"))
-        ro_widget("phase", "Phase", project.get("Phase"))
-        ro_widget("iris", "IRIS", project.get("IRIS"))
-        ro_widget("stip", "STIP", project.get("STIP"))
-        ro_widget("fed_proj_num", "Federal Project Number", project.get("Fed_Proj_Num"))
+        c1, c2 = st.columns(2)
+        c1.ro_widget = ro_widget(
+            "construction_year",
+            "Construction Year",
+            project.get("Construction_Year"),
+        )
+        c2.ro_widget = ro_widget(
+            "phase",
+            "Phase",
+            project.get("Phase"),
+        )
 
+        c3, c4, c5 = st.columns(3)
+        ro_widget("iris", "IRIS", fmt_string(project.get("IRIS")), container=c3)
+        ro_widget("stip", "STIP", fmt_string(project.get("STIP")), container=c4)
+        ro_widget(
+            "fed_proj_num",
+            "Federal Project Number",
+            fmt_string(project.get("Fed_Proj_Num")),
+            container=c5,
+        )
+
+        st.write("")
+
+        # --------------------------------------------------------------
         st.markdown("<h5>3. FUNDING TYPE & PRACTICE</h5>", unsafe_allow_html=True)
-        ro_widget("fund_type", "Funding Type", project.get("Fund_Type"))
-        ro_widget("proj_prac", "Project Practice", project.get("Proj_Prac"))
+        c1, c2 = st.columns(2)
+        ro_widget("fund_type", "Funding Type", project.get("Fund_Type"), container=c1)
+        ro_widget("proj_prac", "Project Practice", project.get("Proj_Prac"), container=c2)
 
+        st.write("")
+
+        # --------------------------------------------------------------
         st.markdown("<h5>4. START & END DATE</h5>", unsafe_allow_html=True)
-        ro_widget("anticipated_start", "Anticipated Start", fmt_date(project.get("Anticipated_Start")))
-        ro_widget("anticipated_end", "Anticipated End", fmt_date(project.get("Anticipated_End")))
+        c1, c2 = st.columns(2)
+        ro_widget(
+            "anticipated_start",
+            "Anticipated Start",
+            fmt_date(project.get("Anticipated_Start")),
+            container=c1,
+        )
+        ro_widget(
+            "anticipated_end",
+            "Anticipated End",
+            fmt_date(project.get("Anticipated_End")),
+            container=c2,
+        )
 
+        st.write("")
+
+        # --------------------------------------------------------------
         st.markdown("<h5>5. AWARD INFORMATION</h5>", unsafe_allow_html=True)
-        ro_widget("award_date", "Award Date", fmt_agol_date(project.get("Award_Date")))
-        ro_widget("award_fiscal_year", "Awarded Fiscal Year", fmt_int(project.get("Award_Fiscal_Year"), year=True))
-        ro_widget("contractor", "Awarded Contractor", project.get("Contractor"))
-        ro_widget("awarded_amount", "Awarded Amount", fmt_currency(project.get("Awarded_Amount")))
-        ro_widget("current_contract_amount", "Current Contract Amount", fmt_currency(project.get("Current_Contract_Amount")))
-        ro_widget("amount_paid_to_date", "Amount Paid to Date", fmt_currency(project.get("Amount_Paid_To_Date")))
-        ro_widget("tenadd", "Tentative Advertise Date", fmt_date(project.get("TenAdd")))
+        c1, c2 = st.columns(2)
+        ro_widget(
+            "award_date",
+            "Award Date",
+            fmt_agol_date(project.get("Award_Date")),
+            container=c1,
+        )
+        ro_widget(
+            "award_fiscal_year",
+            "Awarded Fiscal Year",
+            fmt_int(project.get("Award_Fiscal_Year"), year=True),
+            container=c2,
+        )
 
+        ro_widget(
+            "contractor",
+            "Awarded Contractor",
+            fmt_string(project.get("Contractor")),
+        )
+
+        c3, c4, c5 = st.columns(3)
+        ro_widget(
+            "awarded_amount",
+            "Awarded Amount",
+            fmt_currency(project.get("Awarded_Amount")),
+            container=c3,
+        )
+        ro_widget(
+            "current_contract_amount",
+            "Current Contract Amount",
+            fmt_currency(project.get("Current_Contract_Amount")),
+            container=c4,
+        )
+        ro_widget(
+            "amount_paid_to_date",
+            "Amount Paid to Date",
+            fmt_currency(project.get("Amount_Paid_To_Date")),
+            container=c5,
+        )
+
+        ro_widget(
+            "tenadd",
+            "Tentative Advertise Date",
+            fmt_date(project.get("TenAdd")),
+        )
+
+        st.write("")
+
+        # --------------------------------------------------------------
         st.markdown("<h5>6. DESCRIPTION</h5>", unsafe_allow_html=True)
         if is_awp:
-            ro_widget("awp_proj_desc", "AASHTOWare Description", project.get("AWP_Proj_Desc"), textarea=True)
-        ro_widget("proj_desc", "Public Description", project.get("Proj_Desc"), textarea=True)
+            ro_widget(
+                "awp_proj_desc",
+                "AASHTOWare Description",
+                fmt_string(project.get("AWP_Proj_Desc")),
+                textarea=True,
+            )
 
+        ro_widget(
+            "proj_desc",
+            "Public Description",
+            fmt_string(project.get("Proj_Desc")),
+            textarea=True,
+        )
+
+        st.write("")
+
+        # --------------------------------------------------------------
         st.markdown("<h5>7. CONTACT</h5>", unsafe_allow_html=True)
-        ro_widget("contact_name", "Contact Name", project.get("Contact_Name"))
-        ro_widget("contact_role", "Role", project.get("Contact_Role"))
-        ro_widget("contact_email", "Email", project.get("Contact_Email"))
-        ro_widget("contact_phone", "Phone", project.get("Contact_Phone"))
+        ro_widget(
+            "contact_name",
+            "Contact",
+            fmt_string(project.get("Contact_Name")),
+        )
 
-        st.markdown("<h5>8. ROUTE INFORMATION</h5>", unsafe_allow_html=True)
-        ro_widget("route_id", "Route ID", project.get("Route_ID"))
-        ro_widget("route_name", "Route Name", project.get("Route_Name"))
+        c1, c2 = st.columns(2)
+        ro_widget(
+            "contact_email",
+            "Email",
+            fmt_string(project.get("Contact_Email")),
+            container=c1,
+        )
+        ro_widget(
+            "contact_phone",
+            "Phone",
+            fmt_string(project.get("Contact_Phone")),
+            container=c2,
+        )
 
-        st.markdown("<h5>9. IMPACTED COMMUNITIES</h5>", unsafe_allow_html=True)
-        ro_widget("impact_comm", "Impacted Communities", project.get("Impact_Comm"))
-        ro_widget("impact_comm_names", "Community Names", project.get("Impact_Comm_Names"))
+        st.write("")
 
-        st.markdown("<h5>10. PROJECT LINKS / IDS</h5>", unsafe_allow_html=True)
-        ro_widget("proj_web", "Project Website", project.get("Proj_Web"))
-        ro_widget("aashto_id", "AASHTO ID", project.get("aashto_id"))
-        ro_widget("aashto_selected_project", "AASHTOWare Selection", project.get("aashto_selected_project"))
+        # --------------------------------------------------------------
+        st.markdown("<h5>8. WEB LINK</h5>", unsafe_allow_html=True)
+        ro_widget(
+            "proj_web",
+            "Project Website",
+            fmt_string(project.get("Proj_Web")),
+        )
 
 
 # -----------------------------------------------------------------------------
